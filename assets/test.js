04bc0d36beccaba2dcfe0d7f0338484f2d7c22df
Jen Driggers <jen@repairpal.com>
2014-09-12 09:49:12 -0700

    first header

--- assets/test.js
+++ assets/test.js
@@ -1,1 +1,13 @@
 // alert('hello');
+
+$(document).ready(function(){
+	$('body').addClass('js');
+	var $menu = $('#menu'),
+	$menulink = $('.menu-link');
+
+	$menulink.click(function(){
+		$menulink.toggleClass('active');
+		$menu.toggleClass('active');
+		return false;
+	});
+});
